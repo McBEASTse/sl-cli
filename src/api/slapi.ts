9fcafbd5f723a1ae0161428138a163b5f281330a
Marcus Pettersson <marcus.pettersson@dtms.se>
2026-09-01 16:28:22 +0200

    Some styling depending on line number, needs to be more flexible

--- src/api/slapi.ts
+++ src/api/slapi.ts
@@ -1,5 +1,5 @@
 import type { JourneyPlanner } from "./slapi-types.js";
-import kleur from "kleur";
+import chalk from "chalk";
 import { fetchStationGid } from "../fetch_station.js";
 import { convertTime } from "../convert_time.js";
 import { fetchDeparturesFromSite } from "../fetch_sites.js";
@@ -92,21 +92,80 @@ function formatLeg(leg: any): string | null {
   const destination = leg.destination.name;
 
   let lineLabel = lineNumber;
-  if (lineNumber === "10" || lineNumber === "11") {
-    lineLabel = kleur.bold().black().bgBlue(` T${lineNumber} `);
-  } else if (lineNumber === "13" || lineNumber === "14") {
-    lineLabel = kleur.bold().black().bgRed(` T${lineNumber} `);
-  } else if (lineNumber === "29") {
-    lineLabel = kleur.bold().black().bgMagenta(` R${lineNumber} `);
-  } else if (
-    lineNumber === "17" ||
-    lineNumber === "18" ||
-    lineNumber === "19"
-  ) {
-    lineLabel = kleur.bold().black().bgGreen(` ${lineNumber} `);
-  } else {
-    lineLabel = kleur.bold().white().bgBlack(` B${lineNumber} `);
+  if (productName.includes() === "Waxholm") {
+    lineLabel =
+      chalk.hex("#FFFFFF").bold.bgHex("#000000")(` W `) +
+      chalk.hex("#FFFFFF").bold.bgHex("#498228")(` ${lineNumber} `);
   }
-
+  switch (lineNumber) {
+    case "7":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` SC `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#747770")(` ${lineNumber} `);
+      break;
+    case "10":
+    case "11":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` T `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#007DB8")(` ${lineNumber} `);
+      break;
+    case "12":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` N `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#627892")(` ${lineNumber} `);
+      break;
+    case "13":
+    case "14":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` T `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#D71D24")(` ${lineNumber} `);
+      break;
+    case "17":
+    case "18":
+    case "19":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` T `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#148541")(` ${lineNumber} `);
+      break;
+    case "21":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` L `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#007DB8")(` ${lineNumber} `);
+      break;
+    case "25":
+    case "26":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` S `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#007DB8")(` ${lineNumber} `);
+      break;
+    case "27":
+    case "28":
+    case "29":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` R `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#9F599A")(` ${lineNumber} `);
+      break;
+    case "30":
+    case "31":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` Tv `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#007DB8")(` ${lineNumber} `);
+      break;
+    case "40":
+    case "41":
+    case "42":
+    case "43":
+    case "44":
+    case "48":
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` P `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#CC417F")(` ${lineNumber} `);
+      break;
+    default:
+      lineLabel =
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` B `) +
+        chalk.hex("#FFFFFF").bold.bgHex("#000000")(` ${lineNumber} `);
+      break;
+  }
   return `${origin} ${lineLabel} => ${destination}`;
 }
